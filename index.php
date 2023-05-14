9cdf62cbb0497a0da5de5fa82fc769a7099cf83d
Luzzardd <mnaufalhasyim112@gmail.com>
2023-05-15 01:52:31 +0700

    pert19-20

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+require 'functions.php';
+// mengambil data / query data
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Pengguna</title>
+</head>
+<body>
+    <h1>Data Pengguna</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No</th>
+        <th>Aksi</th>
+        <th>Avatar</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Role</th>
+
+    </tr>
+    <?php $i = 1 ?>
+    <?php foreach($mahasiswa as $row): ?>
+    <tr>
+        <td><?= $i ?></td>
+        <td>
+            <a href="">Ubah</a> |
+            <a href="">Hapus</a>
+
+
+        </td>
+        <td><img src="img/<?php echo $row["gambar"] ?>"  width="50" alt=""></td>
+        <td><?= $row ["nama"];?></td>
+        <td><?= $row ["email"];?></td>
+        <Td><?= $row ["phone"];?></Td>
+        <td><?= $row ["role"];?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+    </table><br>
+    <a href="tambah.php"> Tambah pengguna</a>
+    
+</body>
+</html>
---
+++ tambah.php
@@ -0,0 +1,60 @@
+<?php 
+$conn = mysqli_connect("localhost", "root","","pertemuan19");
+if ( isset($_POST["submit"])){
+    //mengambil data dari tiap elemen
+        $gambar = $_POST["gambar"];
+        $nama = $_POST["nama"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $role = $_POST["role"];
+
+    //query insert data
+    $query = "INSERT INTO mahasiswa VALUES ('','$gambar','$nama','$email,'$phone','$role')";
+    
+    mysqli_query($conn, $query);
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+</head>
+<body>
+    <h1>Tambah Data</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nama">Nama :</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">Email :</label>
+                <input type="text" name="email" id="Email">
+            </li>
+            <li>
+                <label for="phone">Phone :</label>
+                <input type="text" name="phone" id="phone">
+            </li>
+            <li>
+                <label for="role">Role :</label>
+                <input type="text" name="role" id="role">
+
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah</button>
+            </li>
+        </ul>
+
+    </form>
+    
+</body>
+</html>
