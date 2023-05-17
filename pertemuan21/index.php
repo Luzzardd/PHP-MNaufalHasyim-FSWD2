b31642edacde2e0477c92dd43a4d513e0d9a4166
Luzzardd <mnaufalhasyim112@gmail.com>
2023-05-17 19:17:23 +0700

    pert21

---
+++ pertemuan21/index.php
@@ -0,0 +1,42 @@
+<?php
+class animal
+{
+    public $nama,
+        $jenis;
+    public function __construct($nama, $jenis){
+        $this->nama = $nama;
+        $this-> jenis = $jenis;
+    }
+    public function getinfo()
+    {
+    echo "Hewan ini adalah  {$this->nama} jenis {$this->jenis}";
+    }
+}
+class cat extends animal{
+    public function __construct($nama)
+    {
+        $this->nama = $nama;
+    }
+    
+    public function getinfo()
+    {
+    echo "hewan ini adalah {$this->nama} jenis kucing. kucing adalah hewan yang suka bermain dan bersih.";
+    }
+}
+class dog extends animal
+{
+    public function __construct($nama)
+    {
+        $this->nama = $nama;
+    }
+    public function getinfo()
+    {
+        echo "Hewan ini adalah {$this->nama} jenis anjing. Anjing adalah hewan yang setia dan cerdas.";
+    }
+}
+$animal = new animal(" harimau","karnivora");
+echo $animal->getinfo(). "<br>";
+$cat = new cat("Kitty");
+echo $cat->getinfo() .  "<br>";
+$dog = new dog("buddy");
+echo $dog->getinfo() . "<br>";
